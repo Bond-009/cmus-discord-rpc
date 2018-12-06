32f01217c7f241568e1441fdd761354ec0e3d88e
Bond_009 <bond.009@outlook.com>
2018-12-06 15:06:33 +0100

    Use filename when artist and title field are empty
    Get the path to the socket the same way as cmus itself

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use discord_rpc_client::Client;
 use discord_rpc_client::models::Activity;
 use regex::Regex;
 
-use std::{env, thread, time};
-use std::io::{BufReader,Error, ErrorKind, prelude::*};
+use std::{env, thread};
+use std::io::{BufRead, BufReader,Error, ErrorKind, Write};
 use std::os::unix::net::UnixStream;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 #[derive(PartialEq, Debug)]
 enum Status {
@@ -18,9 +18,9 @@ enum Status {
 }
 
 fn main() {
-
-    let mut stream = get_unixstream();
-    let mut drpc = Client::new(431179120836214795).expect("Failed to start client");
+    let socket_path = get_socket_path();
+    let mut stream = get_unix_stream(&socket_path);
+    let mut drpc = Client::new(431179120836214795);
     drpc.start();
 
     loop {
@@ -28,34 +28,35 @@ fn main() {
             Ok(_) => (),
             Err(_) => {
                 drpc.clear_activity().expect("Failed clear presence");
-                stream = get_unixstream();
+                stream = get_unix_stream(&socket_path);
                 continue;
             }
         }
-        let reader = BufReader::new(&mut stream);
-
+        let mut reader = BufReader::new(&stream);
         let mut output = String::new();
-        for line in reader.lines() {
-            match line {
-                Ok(line) => {
-                    if line == "" {
-                        break;
-                    }
-                    output += &line;
-                    output += &"\n".to_string();
-                }
-                Err(x) => panic!(x)
-            }
-        }
+        // Read until an empty line
+        while reader.read_line(&mut output).unwrap() != 1 {};
 
         let status = get_status(&get_value(&output, "status")).unwrap();
 
-        let mut ac: Activity = Activity::new().details(format!("{:?}", status));
-
+        let mut ac = Activity::new()
+                        .details(format!("{:?}", status));
         if status != Status::Stopped {
             let artist = get_value(&output, "tag artist");
             let title = get_value(&output, "tag title");
-            ac = ac.state(artist + " - " + &title);
+            
+            if artist.is_empty() || title.is_empty() {
+                // Capture filename
+                let file_r = Regex::new(r"(?m)^file .+/(?P<f>.+)\..+\n").unwrap();
+                let file = match file_r.captures(&output) {
+                    Some(v) => v["f"].to_owned(),
+                    None => "".to_owned()
+                };
+                ac = ac.state(file);
+            }
+            else {
+                ac = ac.state(artist + " - " + &title);
+            }
 
             if status == Status::Playing {
                 let duration = get_value(&output, "duration").parse::<u64>().unwrap();
@@ -67,29 +68,48 @@ fn main() {
 
         drpc.set_activity(|_| ac).expect("Failed to set presence");
 
-        thread::sleep(time::Duration::from_secs(15));
+        thread::sleep(Duration::from_secs(15));
     }
 }
 
-fn get_unixstream() -> UnixStream {
-    let rdir = env::var("XDG_RUNTIME_DIR").unwrap();
-    let path = rdir + "/cmus-socket";
+fn get_unix_stream(socket_path: &str) -> UnixStream {
     loop {
-        match UnixStream::connect(&path) {
-            Ok(strm) => return strm,
+        match UnixStream::connect(socket_path) {
+            Ok(s) => return s,
             Err(_) => {
                 // Try again in 15 seconds
-                thread::sleep(time::Duration::from_secs(15));
+                thread::sleep(Duration::from_secs(15));
             }
         }
     }
 }
 
+/// Get the path to the cmus socket the same way as cmus itself
+fn get_socket_path() -> String
+{
+    match env::var("CMUS_SOCKET") {
+        Ok(v) => return v,
+        Err(_) => ()
+    };
+
+    match env::var("XDG_RUNTIME_DIR") {
+        Ok(v) => return v + "/cmus-socket",
+        Err(_) => ()
+    };
+
+    let cmus_config_dir = match env::var("XDG_CONFIG_HOME") {
+        Ok(v) => v,
+        Err(_) => env::var("HOME").unwrap() + "/.config"
+    } + "/cmus";
+ 
+    return cmus_config_dir + "/socket"
+}
+
 fn get_value(input: &str, key: &str) -> String {
-    let re = Regex::new(&format!("(?m)^{} (?P<t>.+)$", key)).unwrap();
+    let re = Regex::new(&format!("(?m)^{} (?P<v>.+)$", key)).unwrap();
     match re.captures(input) {
-        Some(value) => value["t"].to_string(),
-        None => "".to_string()
+        Some(value) => value["v"].to_owned(),
+        None => "".to_owned()
     }
 }
 
